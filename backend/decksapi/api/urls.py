65b358fa6e0ed600c8a2c7dfd8eb843e4fd3258b
MikhailDevW <wildmv@gmail.com>
2023-05-19 16:15:08 +0300

    djoser urls changed

--- backend/decksapi/api/urls.py
+++ backend/decksapi/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView
+)
 
 from .views import DashboardViewSet, CardsViewSet
+
 
 router = SimpleRouter()
 router.register('dashboard', DashboardViewSet)
@@ -14,4 +18,21 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    re_path(
+        r"^auth/create/?",
+        TokenObtainPairView.as_view(),
+        name="jwt-create"
+    ),
+    re_path(
+        r"^auth/refresh/?",
+        TokenRefreshView.as_view(),
+        name="jwt-refresh"
+    ),
+    re_path(
+        r"^auth/verify/?",
+        TokenVerifyView.as_view(),
+        name="jwt-verify"
+    ),
+    # path('auth/', include('djoser.urls.jwt')),
 ]
