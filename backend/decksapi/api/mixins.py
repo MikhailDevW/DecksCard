45f722f401cf5f9ed1558a841e993de5c7a71a3a
krokodila888 <krokodila888@yandex.ru>
2023-08-12 10:04:14 +0300

    Revert "fix: fixing frontend errors"
    This reverts commit 16ccf5a25d538da8698f551492bc28f138fc88f1.

--- backend/decksapi/api/mixins.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class CreateViewSet(
-    mixins.CreateModelMixin,
-    viewsets.GenericViewSet
-):
-    pass
--- backend/decksapi/core/image_verify.py
+++
@@ -1,6 +0,0 @@
-def image_verufy():
-    pass
-
-
-if __name__ == '__main__':
-    print('hello')
