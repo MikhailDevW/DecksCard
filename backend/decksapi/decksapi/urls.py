578d8ac85fc42e8529d0097f864cbff05ddbe581
krokodila888 <krokodila888@yandex.ru>
2023-08-12 11:20:57 +0300

    fix: fixing frontend

---
+++ backend/decksapi/decksapi/urls.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+# from django.conf.urls import url
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Decks API",
+#       default_version='v1',
+#       description="Decks API description",
+#       # terms_of_service="URL страницы с пользовательским соглашением",
+#       contact=openapi.Contact(email="email here"),
+#       license=openapi.License(name="BSD License"),
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+# )
+
+# urlpatterns += [
+#    url(r'^swagger(?P<format>\.json|\.yaml)$',
+#        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+#        name='schema-swagger-ui'),
+#    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+#        name='schema-redoc'),
+# ]
